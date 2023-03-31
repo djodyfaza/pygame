c4f63738a944350088efad610c734470ae05a831
Djody Faza <djodyfaza@yahoo.com>
2023-03-31 13:33:28 +0700

    Minor variable update

--- signage.py
+++ signage.py
@@ -1,12 +1,10 @@
 import pygame
-# import signage_blank
-import subprocess
-import os, sys, time
+import os, sys, time, subprocess
+
 
 # Set up display
-display_width = 485
-display_height = 700
-display = pygame.display.set_mode((display_width, display_height))
+dis_hei, dis_wid = 700, 485
+display = pygame.display.set_mode((dis_wid, dis_hei))
 pygame.display.set_caption("Digital Signage")
 
 # Set up images
@@ -16,12 +14,12 @@ current_index = 0
 current_image = pygame.image.load(os.path.join(img_dir, images[current_index]))
 next_index = (current_index + 1) % len(images)
 next_image = pygame.image.load(os.path.join(img_dir, images[next_index]))
-offset = display_width // 10
+offset = dis_wid // 10
 
 # Set up clock
 clock = pygame.time.Clock()
 
-# Set up delay
+# Set up delayx
 delay = 4
 last_transition_time = time.time()
 
@@ -34,31 +32,35 @@ while True:
             quit()
         # Quit window when random clicked
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            os.system('python signage_blank.py')
+            subprocess.Popen(['python', 'signage_blank.py'])
             pygame.quit()
             quit()
-            
 
     # Create two surfaces for transition effect
-    previous_surface = pygame.Surface((display_width, display_height))
+    previous_surface = pygame.Surface((dis_wid, dis_hei))
     previous_surface.blit(current_image, (0, 0))
-    next_surface = pygame.Surface((display_width, display_height))
+    next_surface = pygame.Surface((dis_wid, dis_hei))
     next_surface.blit(next_image, (0, 0))
 
     # Scale images to fit screen
-    current_image = pygame.transform.scale(current_image, (display_width, display_height))
-    next_image = pygame.transform.scale(next_image, (display_width, display_height))
+    current_image = pygame.transform.scale(current_image, (dis_wid, dis_hei))
+    next_image = pygame.transform.scale(next_image, (dis_wid, dis_hei))
 
     # Draw current image
     display.blit(current_image, (0, 0))
 
     # Calculate offset for transition effect
     if offset > 0:
-        previous_surface.set_alpha(int(offset / (display_width / 255)))
-        next_surface.set_alpha(255 - int(offset / (display_width / 255)))
+        previous_surface.set_alpha(int(offset / (dis_wid / 255)))
+        next_surface.set_alpha(255 - int(offset / (dis_wid / 255)))
         display.blit(previous_surface, (-offset, 0))
-        display.blit(next_surface, (display_width - offset, 0))
-        offset -= 5  # kurangi offset sebesar 5 setiap iterasi
+        display.blit(next_surface, (dis_wid - offset, 0))
+        offset -= 5  # Decrease offset for 5 iteration
+
+    elif time.time() - last_transition_time >= delay:
+        current_index = next_index
+        current_image = next_image
+        
 
     # Transition to next image when delay is over
     elif time.time() - last_transition_time >= delay:
@@ -66,7 +68,7 @@ while True:
         current_image = next_image
         next_index = (current_index + 1) % len(images)
         next_image = pygame.image.load(os.path.join(img_dir, images[next_index]))
-        offset = display_width // 10
+        offset = dis_wid // 10
         last_transition_time = time.time()
 
     # Update display
@@ -74,7 +76,3 @@ while True:
 
     # Set frame rate
     clock.tick(25)
-
-if __name__=='__main__':
-	obj=change_image()
-	obj.main()
